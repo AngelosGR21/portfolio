9c31da75f33032da67b7798be74ff489ceaeea93
AngelosGR21 <77233810+AngelosGR21@users.noreply.github.com>
2021-09-01 15:30:42 +0300

    adding navbar for smaller windows and styling

--- src/components/SideNavbar.js
+++ src/components/SideNavbar.js
@@ -1,5 +1,7 @@
+import { useState, useEffect } from "react";
+
 import { motion } from "framer-motion";
-import { Box, SvgIcon, Typography } from "@material-ui/core";
+import { Box, SvgIcon, Typography, AppBar } from "@material-ui/core";
 import CodeIcon from "@material-ui/icons/Code";
 import EmailIcon from "@material-ui/icons/Email";
 import HomeIcon from "@material-ui/icons/Home";
@@ -9,6 +11,143 @@ import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import sideBarStyles from "../styles/sideBarStyles";
 const SideNavbar = () => {
   const classes = sideBarStyles();
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const [openMenu, setOpenMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+
+  const checkSize = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+    if (openMenu && width >= 960) {
+      setOpenMenu(false);
+      setShowMenu(false);
+    }
+    return () => {
+      window.removeEventListener("resize", checkSize);
+    };
+  });
+
+  //Variants for motion
+
+  const topVariant = {
+    clicked: {
+      originX: "left",
+      rotate: openMenu ? "35deg" : "0deg",
+    },
+  };
+
+  const middleVariant = {
+    clicked: {
+      opacity: openMenu ? 0 : 1,
+      transition: {
+        duration: 0.2,
+      },
+    },
+  };
+
+  const bottomVariant = {
+    clicked: {
+      originX: "left",
+      rotate: openMenu ? "-35deg" : "0deg",
+    },
+  };
+
+  const modalVariant = {
+    initial: {
+      clipPath: "circle(0px at 5% -10px)",
+      webkitClipPath: "circle(10px at 5% -10px)",
+      display: "flex",
+      transition: {
+        duration: 0.5,
+      },
+    },
+    clicked: {
+      display: "flex",
+      clipPath: "circle(1000px at 5% -10px)",
+      webkitClipPath: "circle(1000px at 5% -10px)",
+      transition: {
+        duration: 1,
+      },
+    },
+  };
+
+  //Mobile view
+  if (width < 960) {
+    return (
+      <>
+        <AppBar
+          position="fixed"
+          className={classes.mobileNavbar}
+          component="nav"
+        >
+          <div
+            className={classes.mobileMenu}
+            onClick={() => {
+              setOpenMenu((prev) => !prev);
+              setShowMenu((prev) => !prev);
+            }}
+          >
+            <motion.span
+              className={classes.menuLines}
+              variants={topVariant}
+              animate="clicked"
+            ></motion.span>
+            <motion.span
+              className={classes.menuLines}
+              variants={middleVariant}
+              animate="clicked"
+              transition="transition"
+            ></motion.span>
+            <motion.span
+              className={classes.menuLines}
+              variants={bottomVariant}
+              animate="clicked"
+            ></motion.span>
+          </div>
+        </AppBar>
+
+        <Box
+          className={classes.menuModal}
+          component={motion.div}
+          variants={modalVariant}
+          animate={showMenu ? "clicked" : "initial"}
+        >
+          <Box className={classes.menuItemsContainer}>
+            <Typography className={classes.menuItemHeader}>Home</Typography>
+            <SvgIcon classes={{ root: classes.sideBarIcons }}>
+              <HomeIcon />
+            </SvgIcon>
+          </Box>
+          <Box className={classes.menuItemsContainer}>
+            <Typography className={classes.menuItemHeader}>About</Typography>
+            <SvgIcon classes={{ root: classes.sideBarIcons }}>
+              <AccountCircleIcon />
+            </SvgIcon>
+          </Box>
+          <Box className={classes.menuItemsContainer}>
+            <Typography className={classes.menuItemHeader}>Projects</Typography>
+            <SvgIcon classes={{ root: classes.sideBarIcons }}>
+              <CodeIcon />
+            </SvgIcon>
+          </Box>
+          <Box className={classes.menuItemsContainer}>
+            <Typography className={classes.menuItemHeader}>
+              Contact Me
+            </Typography>
+            <SvgIcon classes={{ root: classes.sideBarIcons }}>
+              <EmailIcon />
+            </SvgIcon>
+          </Box>
+        </Box>
+      </>
+    );
+  }
+
+  //Desktop view
   return (
     <>
       <Box className={classes.sideBarContainer} component="section">
--- src/styles/sideBarStyles.js
+++ src/styles/sideBarStyles.js
@@ -3,6 +3,63 @@ import { makeStyles } from "@material-ui/core";
 import colors from "./colors";
 
 const sideBarStyles = makeStyles((theme) => ({
+  //Mobile view
+  mobileNavbar: {
+    height: "50px",
+    backgroundColor: colors.grey,
+    position: "relative",
+  },
+  mobileMenu: {
+    width: "50px",
+    height: "50px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    padding: "10px 8px",
+  },
+  menuLines: {
+    width: "70%",
+    height: "2px",
+    backgroundColor: "black",
+    "&:nth-of-type(2)": {
+      margin: "4px 0",
+    },
+  },
+  menuModal: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100vw",
+    height: "100vh",
+    display: "none",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colors.grey,
+    zIndex: "2",
+  },
+  menuItemsContainer: {
+    display: "flex",
+    color: colors.white,
+    backgroundColor: colors.matteBlack,
+    height: "45px",
+    width: "50%",
+    borderRadius: "5px",
+    padding: "0 20px",
+    alignItems: "center",
+    justifyContent: "space-between",
+    margin: "20px",
+    [theme.breakpoints.up("sm")]: {
+      width: "30%",
+    },
+  },
+  menuItemHeader: {
+    // marginRight: "20px",
+  },
+
+  //Desktop View
+
   sideBarContainer: {
     width: "300px",
     height: "300px",
