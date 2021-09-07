889e6472da28dc4df44dc1915c2963dfec798c0d
AngelosGR21 <77233810+AngelosGR21@users.noreply.github.com>
2021-09-07 22:03:10 +0300

    handling form and adding styles

--- src/Contact.js
+++ src/Contact.js
@@ -1,59 +1,175 @@
+import { useState } from "react";
+import image from "./images/messageSent.svg";
+import emailjs from "emailjs-com";
+import env from "react-dotenv";
+
 import SideNavbar from "./components/SideNavbar";
 
 import globalStyles from "./styles/globalStyles";
 import contactStyles from "./styles/contactStyles";
 
-import { TextField, Button, Box } from "@material-ui/core";
+import { TextField, Button, Box, Typography } from "@material-ui/core";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Contact = () => {
   globalStyles();
   const classes = contactStyles();
+
+  const [success, setSuccess] = useState(false);
+  const [contactForm, setContactForm] = useState(true);
+
+  const [details, setDetails] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    message: "",
+  });
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        `${env.SERVICE_ID}`,
+        `${env.TEMPLATE_ID}`,
+        e.target,
+        `${env.USER_ID}`
+      )
+      .then(
+        () => {
+          setDetails({ name: "", email: "", message: "" });
+          setContactForm(false);
+          setSuccess(true);
+        },
+        (error) => {
+          console.log(error.text, "ERROR");
+        }
+      );
+  };
+
+  const showContact = () => {
+    setContactForm(true);
+    setSuccess(false);
+  };
+
   return (
     <>
       <SideNavbar />
-      <Box className={classes.formContainer}>
-        <form className={classes.form}>
-          <TextField
-            label="Name"
-            required
-            variant="outlined"
-            className={classes.borderChange}
-            InputLabelProps={{
-              className: classes.inputLabel,
-              focused: classes.focused,
-            }}
-          ></TextField>
+      <AnimatePresence>
+        {contactForm && (
+          <Box
+            className={classes.formContainer}
+            component={motion.section}
+            initial={{ y: "-150vw" }}
+            animate={{ y: 0 }}
+            exit={{ y: "150vw" }}
+            transition={{ duration: 2 }}
+          >
+            <Typography variant="h4" className={classes.header}>
+              Contact Form
+            </Typography>
+            <form className={classes.form} onSubmit={sendEmail}>
+              <Box className={classes.namesContainer}>
+                <TextField
+                  autoComplete="off"
+                  name="first_name"
+                  autoFocus={true}
+                  value={details.first_name}
+                  onChange={(e) =>
+                    setDetails({ ...details, first_name: e.target.value })
+                  }
+                  label="First Name"
+                  required
+                  variant="outlined"
+                  className={`${classes.borderChange} ${classes.firstName}`}
+                  InputLabelProps={{
+                    className: classes.inputLabel,
+                  }}
+                ></TextField>
+                <TextField
+                  name="last_name"
+                  value={details.last_name}
+                  onChange={(e) =>
+                    setDetails({ ...details, last_name: e.target.value })
+                  }
+                  label="Last Name"
+                  required
+                  variant="outlined"
+                  className={`${classes.borderChange} ${classes.lastName}`}
+                  InputLabelProps={{
+                    className: classes.inputLabel,
+                  }}
+                ></TextField>
+              </Box>
+              <TextField
+                name="email"
+                value={details.email}
+                onChange={(e) =>
+                  setDetails({ ...details, email: e.target.value })
+                }
+                label="Email"
+                required
+                variant="outlined"
+                className={`${classes.emailInput} ${classes.borderChange}`}
+                InputLabelProps={{
+                  className: classes.inputLabel,
+                }}
+              ></TextField>
 
-          <TextField
-            label="Email"
-            required
-            variant="outlined"
-            className={`${classes.emailInput} ${classes.borderChange}`}
-            InputLabelProps={{
-              className: classes.inputLabel,
-              focused: classes.focused,
-            }}
-          ></TextField>
+              <TextField
+                name="message"
+                value={details.message}
+                onChange={(e) =>
+                  setDetails({ ...details, message: e.target.value })
+                }
+                label="Message"
+                required
+                variant="outlined"
+                multiline
+                minRows={2}
+                maxRows={5}
+                className={classes.borderChange}
+                InputLabelProps={{
+                  className: classes.inputLabel,
+                }}
+              ></TextField>
+              <Box className={classes.buttonContainer}>
+                <Button
+                  variant="contained"
+                  type="submit"
+                  className={classes.button}
+                >
+                  Send
+                </Button>
+              </Box>
+            </form>
+          </Box>
+        )}
+      </AnimatePresence>
 
-          <TextField
-            label="Message"
-            required
-            variant="outlined"
-            multiline
-            minRows={2}
-            className={classes.borderChange}
-            InputLabelProps={{
-              className: classes.inputLabel,
-              focused: classes.focused,
-            }}
-          ></TextField>
-          <Box>
-            <Button variant="contained" type="submit" color="secondary">
-              Submit
+      <AnimatePresence>
+        {success && (
+          <Box
+            className={classes.container}
+            component={motion.section}
+            initial={{ y: "-150vw" }}
+            animate={{ y: 0 }}
+            exit={{ y: "150vw" }}
+            transition={{ duration: 2 }}
+          >
+            <img alt="message sent" src={image} className={classes.image}></img>
+            <Typography className={classes.message} variant="h4">
+              Message sent
+            </Typography>
+            <Button
+              variant="contained"
+              className={classes.backButton}
+              onClick={showContact}
+            >
+              Go back
             </Button>
           </Box>
-        </form>
-      </Box>
+        )}
+      </AnimatePresence>
     </>
   );
 };
--- src/styles/contactStyles.js
+++ src/styles/contactStyles.js
@@ -4,31 +4,82 @@ import colors from "./colors";
 
 const contactStyles = makeStyles((theme) => ({
   formContainer: {
+    position: "absolute",
     display: "flex",
     width: "100%",
     height: "100%",
+    flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
   },
   form: {
-    width: "40%",
+    width: "90%",
     padding: "10px",
-    backgroundColor: colors.white,
     height: "300px",
     display: "flex",
     flexDirection: "column",
     justifyContent: "space-evenly",
+    [theme.breakpoints.up("sm")]: {
+      width: "80%",
+    },
+    [theme.breakpoints.up(700)]: {
+      width: "70%",
+    },
+    [theme.breakpoints.up("md")]: {
+      width: "60%",
+    },
+    [theme.breakpoints.up("lg")]: {
+      width: "600px",
+    },
+    [theme.breakpoints.up("xl")]: {
+      width: "700px",
+      transition: "all 0.5s ease-in",
+      transform: "scale(1.5)",
+      marginTop: "8rem",
+    },
   },
+  header: {
+    color: colors.orange,
+    [theme.breakpoints.up("xl")]: {
+      transition: "all 0.5s ease-in",
+      transform: "scale(2.5)",
+      marginBottom: "1rem",
+    },
+  },
+  namesContainer: {
+    display: "flex",
+  },
+
   inputLabel: {
-    color: colors.matteBlack,
+    color: colors.orange,
+
     "&.Mui-focused": {
       color: colors.orange,
+    },
+    [theme.breakpoints.up("xl")]: {
+      fontSize: "1.5rem",
+    },
+    "&.MuiInputLabel-outlined.MuiInputLabel-shrink": {
+      [theme.breakpoints.up("xl")]: {
+        transform: "translate(7px, -10px ) scale(0.75)",
+      },
     },
   },
 
   borderChange: {
+    "& .MuiInputBase-input": {
+      color: colors.white,
+      [theme.breakpoints.up("xl")]: {
+        fontSize: "2rem",
+      },
+    },
+    "& .MuiInputBase-root": {
+      [theme.breakpoints.up("xl")]: {
+        marginBottom: "1rem",
+      },
+    },
     "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
-      borderColor: colors.matteBlack,
+      borderColor: colors.orange,
     },
     "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
       borderColor: colors.orange,
@@ -38,9 +89,57 @@ const contactStyles = makeStyles((theme) => ({
     },
   },
 
+  firstName: {
+    width: "100%",
+    marginRight: "10px",
+    [theme.breakpoints.up("sm")]: {
+      width: "90%",
+    },
+  },
+  lastName: {
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "90%",
+    },
+  },
+
   emailInput: {
-    width: "50%",
     color: colors.white,
+  },
+  buttonContainer: {
+    alignSelf: "end",
+  },
+  button: {
+    backgroundColor: colors.grey,
+    color: colors.orange,
+    "&:hover": {
+      backgroundColor: "#545252",
+    },
+  },
+
+  container: {
+    position: "absolute",
+    width: "100%",
+    height: "85%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: "250px",
+    marginBottom: "1rem",
+  },
+  message: {
+    color: "green",
+    marginBottom: "1rem",
+  },
+  backButton: {
+    backgroundColor: colors.grey,
+    color: colors.white,
+    "&:hover": {
+      backgroundColor: "#545252",
+    },
   },
 }));
 
