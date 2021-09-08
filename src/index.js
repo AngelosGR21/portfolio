5e01ecade330ff85c36d172e2a6b17271514ba97
AngelosGR21 <77233810+AngelosGR21@users.noreply.github.com>
2021-09-08 21:42:40 +0300

    updating index with routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+//Components
 import Home from "./Home";
+import About from "./About";
+import Projects from "./Projects";
+import Contact from "./Contact";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/projects">
+        <Projects />
+      </Route>
+      <Route path="/contact">
+        <Contact />
+      </Route>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
