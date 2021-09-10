d84e9e6f8022f96e2e5623c4b07ae1144f36a63c
AngelosGR21 <77233810+AngelosGR21@users.noreply.github.com>
2021-09-10 12:54:57 +0300

    adding data for the projects

---
+++ src/utils/data.js
@@ -0,0 +1,35 @@
+import ipTracker from "../images/IPTracker.png";
+import portfolio from "../images/portfolio.png";
+import swordShop from "../images/swordShop.png";
+
+const data = [
+  {
+    name: "IP Address Tracker",
+    github: "https://github.com/AngelosGR21/ip-address-tracker",
+    liveWebsite: "https://angelosgr21.github.io/ip-address-tracker/",
+    description:
+      "Users should be able to introduce a domain/ipv4/ipv6 address and view the location, timezone and internet service provider. If no IP/domain was introduced or the address cannot be found it will automatically take your ip and display the details.",
+    image: ipTracker,
+    builtWith: ["React", "Material-UI"],
+  },
+  {
+    name: "Portfolio",
+    github: "https://github.com/AngelosGR21/portfolio",
+    liveWebsite: "https://angelosgr21.github.io/portfolio",
+    description:
+      "The portfolio is the first project i started implementing Framer-Motion and styling more in-depth Material-UI components. ",
+    image: portfolio,
+    builtWith: ["React", "Material-UI", "Framer Motion"],
+  },
+  {
+    name: "Sword Shop",
+    github: "https://github.com/AngelosGR21/anime-swords-shop",
+    liveWebsite: "https://anime-swords-shop.herokuapp.com/",
+    description:
+      "Users should be able to add/remove items, as well as increase or decrement the number of items in the cart page. The app is hosted on heroku so please wait 1-2 minutes until it loads.",
+    image: swordShop,
+    builtWith: ["React", "Material-UI"],
+  },
+];
+
+export default data;
