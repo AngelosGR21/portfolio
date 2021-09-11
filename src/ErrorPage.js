690e766abc3d62dea08a6a9608d6d294a2d3ef6f
AngelosGR21 <77233810+AngelosGR21@users.noreply.github.com>
2021-09-11 22:46:42 +0300

    adding 404 page

---
+++ src/ErrorPage.js
@@ -0,0 +1,30 @@
+import SideNavbar from "./components/SideNavbar";
+import image from "./images/ErrorAlienSpaceship.svg";
+
+import { Link } from "react-router-dom";
+import { Box, Typography, Button } from "@material-ui/core";
+
+import globalStyles from "./styles/globalStyles";
+import errorPageStyles from "./styles/errorPageStyles";
+
+const ErrorPage = () => {
+  globalStyles();
+  const classes = errorPageStyles();
+
+  return (
+    <>
+      <SideNavbar />
+      <Box className={classes.container}>
+        <img src={image} className={classes.image}></img>
+        <Typography className={classes.header}>Page not found!</Typography>
+        <Link to="/" className={classes.link}>
+          <Button variant="outlined" className={classes.button}>
+            Go Back
+          </Button>
+        </Link>
+      </Box>
+    </>
+  );
+};
+
+export default ErrorPage;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 //Components
 import Home from "./Home";
 import About from "./About";
 import Projects from "./Projects";
 import Contact from "./Contact";
+import ErrorPage from "./ErrorPage";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/about">
-        <About />
-      </Route>
-      <Route path="/projects">
-        <Projects />
-      </Route>
-      <Route path="/contact">
-        <Contact />
-      </Route>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/projects">
+          <Projects />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="*">
+          <ErrorPage />
+        </Route>
+      </Switch>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
